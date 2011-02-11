0c5b45705be4079161efbba8d477ab8c509e5d38
Marcel Caraciolo <caraciol@gmail.com>
2011-02-11 16:45:33 -0300

    Adicionado primeiro desafio do PUG-PE 11/02/2011

---
+++ desafio_20110211/desafio.py
@@ -0,0 +1,42 @@
+"""
+Desafio PUG-PE  
+ID: 1
+Semana: 11/02/2011
+
+Problema:
+
+    Dado uma lista de elementos, o objetivo eh converter esta lista em uma lista de sub-listas de elementos consecutivos duplicados. 
+    >>> x = ['a','a','a','a','b','c','c','a','a','d','e','e','e','e']
+    >>> ret = pack(x)
+    >>> ret
+    >>> [['a','a','a','a'],['b'],['c','c'],['a','a'],['d'],['e','e','e','e']]
+    >>> x = ['a', 'b', 'c']
+    >>> ret = pack(x)
+    >>> ret 
+    >>>[['a'], ['b'], ['c']]
+    >>> x = []
+    >>> ret = pack(x)
+    >>> ret
+    >>> []
+     
+  Seu trabalho eh construir essa lista de elementos.  Favor utilizar Testes usando doctest ou UnitTest para validar sua solucao.
+
+"""
+
+
+
+import unittest
+
+
+class Desafio1(unittest.TestCase):
+	
+	def test_pack_duplicates(self):
+		sampleList = ['a','a','a','a','b','c','c','a','a','d','e','e','e','e']
+    		self.assertEqual(
+                     [['a','a','a','a'],['b'],['c','c'],['a','a'],['d'],['e','e','e','e']],
+                     pack(sampleList)                     
+                     )	
+
+
+if __name__ == '__main__':
+	unittest.main()	
