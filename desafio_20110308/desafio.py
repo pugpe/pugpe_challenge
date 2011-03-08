3ae32e76138ece143e54ebb8923e51167a0ccd9e
Marcel Caraciolo <caraciol@gmail.com>
2011-03-08 14:05:18 -0300

    Adicionado o IV Desafio do PUG-PE Challenge.

---
+++ desafio_20110308/desafio.py
@@ -0,0 +1,60 @@
+"""
+Desafio PUG-PE  
+ID: 4
+Semana: 08/03/2011
+Formulado por : Marcel Caraciolo
+
+Problema:
+O objetivo deste problema eh transpor uma matriz.
+
+
+    Seu trabalho eh construir uma funcao transpose que receba uma matriz (em formato de lista de listas) e 
+    retorna a matriz transposta desta (em lista de listas).
+
+    Uma matriz transposta eh o resultado da troca de linhas por colunas em uma determinada matriz.
+
+    EX:
+       | 1  2 |                      | 1 3 5 |
+       | 3  4 |   -> Tranposta ->    | 2 4 6 |
+       | 5  6 |
+
+    >>> ret = tranpose([])
+    >>> ret
+    []
+    >>> ret = tranpose([1])
+    >>> ret
+    [1]
+    >>> ret = transpose([1,2])  
+    >>> ret
+    [[1],[2]]
+    >>> ret = tranpose([[1,2], [3,4], [5,6]])
+    >>> ret
+    [[1,3,5], [2,4,6]]
+     
+  Seu trabalho eh construir essa funcao.  Favor utilizar Testes usando doctest ou UnitTest para validar sua solucao.
+
+"""
+
+
+
+import unittest
+
+
+class Desafio3(unittest.TestCase):
+
+    def test_empty_list(self):
+        self.assertEqual([],
+                    transpose([]))
+
+    def test_unit_list(self):
+        self.assertEqual([1], tranpose([1]))
+
+    def test_simple_List(self):
+        self.assertEqual([[1],[2]], tranpose([1,2]))
+
+    def test_complex_list(self):    
+        self.assertEqual([[1,3,5],[2,4,6]], tranpose([[1,2], [3,4], [5,6]]) )
+
+
+if __name__ == '__main__':
+    unittest.main()    
