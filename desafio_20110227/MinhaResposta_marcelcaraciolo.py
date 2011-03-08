26f173b682547362ee94cf4942031d8f4ecb183d
Marcel Caraciolo <caraciol@gmail.com>
2011-03-08 13:06:27 -0300

    Adicionado resposta de marcelcaraciolo.

---
+++ desafio_20110227/MinhaResposta_marcelcaraciolo.py
@@ -0,0 +1,60 @@
+"""
+Desafio PUG-PE  
+ID: 3
+Semana: 27/02/2011
+Formulado por : Marcel Caraciolo
+
+Problema:
+O objetivo deste problema eh converter uma lista de elementos em uma lista de tuplas de 2 elementos.
+
+
+    Seu trabalho eh construir uma funcao tuplify que receba uma lista como parametro. O retorno dela deve ser esta lista tuplificada.
+    
+    >>> ret = tuplify([])
+    >>> ret
+    []
+    >>> ret = tuplify([1])
+    >>> ret
+    None
+    >>> ret = tuplify([1,2])
+    >>> ret
+    [(1,2)]
+    >>> ret = tuplify(["x",1,"y",2])
+    >>> ret
+    [('x',1), ('y',2)]
+    >>> ret = tuplify([1,2,3])
+    >>> ret
+    [(1,2)]
+
+     
+  Seu trabalho eh construir essa funcao.  Favor utilizar Testes usando doctest ou UnitTest para validar sua solucao.
+
+"""
+
+
+tuplify = lambda l:  None if not l or len(l) <=1 else zip( *[l[i::2] for i in (0,1)] )
+
+
+import unittest
+
+
+class Desafio2(unittest.TestCase):
+
+    def test_empty_list(self):
+        self.assertEqual(None,
+                    tuplify([]))
+
+    def test_unit_list(self):
+        self.assertEqual(None, tuplify([1]))
+
+    def test_simple_List(self):
+        self.assertEqual([(1,2)], tuplify([1,2]))
+
+    def test_complex_list(self):    
+        self.assertEqual([('x',1),('y',2)], tuplify(['x',1,'y',2]))
+
+    def test_incomplete_list(self):
+        self.assertEqual([(1,2)], tuplify([1,2,3]))
+
+if __name__ == '__main__':
+    unittest.main()    
