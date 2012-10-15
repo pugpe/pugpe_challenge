c0649e0389948b7a558afedb6be17cae038e8323
Renato Oliveira <renatooliveira.cin@gmail.com>
2012-10-14 23:48:55 -0300

    Mais testes adicionados

--- desafio_20121014/desafio.py
+++ desafio_20121014/desafio.py
@@ -27,7 +27,10 @@ class Desafio7(unittest.TestCase):
         set_2 = ['a', 'b', 'c']
         set_3 = ['']
         set_4 = ['a']
+        set_5 = []
         self.assertEqual(['', 'a', 'b', ('a', 'b')], subset(set_1))
         self.assertEqual(['', 'a', 'b', 'c', ('a', 'b'), ('a', 'c'), ('b', 'c'), ('a', 'b', 'c')], subset(set_2))
         self.assertEqual([''], subset(set_3))
         self.assertEqual(['', 'a'], subset(set_4))
+        self.assertEqual([], subset(set_5))
+
