f7d1265b6e53764810454c4517a85e8d9eb09bcc
Daker Fernandes Pinheiro <dakerfp@gmail.com>
2012-10-16 19:50:56 -0300

    Update desafio_20121014/desafio.py

--- desafio_20121014/desafio.py
+++ desafio_20121014/desafio.py
@@ -7,38 +7,57 @@ SEMANA: 15/10/2012
 
 Problema: Achar todas as partições de uma dada lista de strings.
     Ex:
-    >>> x = ['a', 'b']
-    >>> ret = subset(x)
-    >>> ret
-    >>> [[''], ['a'], ['b'], ['a', 'b']]
-    >>> y = ['a', 'b', 'c']
-    >>> ret = subset()
-    >>> ret
-    >>> [[''], ['a'], ['b'], ['c'], ['a', 'b'], ['a', 'c'], ['b', 'c'], ['a', 'b', 'c']]
+    >>> x = {'a', 'b'}
+    >>> powerset(x)
+    {frozenset({''}), frozenset({'a'}), frozenset({'b'}), frozenset({'a', 'b'})}
+    >>> y = {'a', 'b', 'c'}
+    >>> ret = powerset(y)
+    {frozenset({''}), frozenset({'a'}), frozenset({'b'}), frozenset({'c'}), frozenset({'a', 'b'}), frozenset({'a', 'c'}), frozenset({'b', 'c'}), frozenset({'a', 'b', 'c'})}
 
     Favor utilizar Testes usando doctest ou UnitTest para validar sua solucao.
 """
 
 import unittest
+import itertools
 
+def iterpowerset(s):
+    l = list(s)
+    for n in range(len(l)+1):
+        for c in itertools.combinations(l, n):
+            yield c
 
-def subset(set):
-    return
+def powerset(set):
+    return {frozenset(s) for s in iterpowerset(set)}
 
 
 class Desafio7(unittest.TestCase):
 
-    def test_find_subset(self):
-        set_1 = ['a', 'b']
-        set_2 = ['a', 'b', 'c']
-        set_3 = ['']
-        set_4 = ['a']
-        set_5 = []
-        self.assertEqual([[''], ['a'], ['b'], ['a', 'b']], subset(set_1))
-        self.assertEqual([[''], ['a'], ['b'], ['c'], ['a', 'b'], ['a', 'c'], ['b', 'c'], ['a', 'b', 'c']], subset(set_2))
-        self.assertEqual([['']], subset(set_3))
-        self.assertEqual([[''], ['a']], subset(set_4))
-        self.assertEqual([], subset(set_5))
+    def test_find_powerset(self):
+        set_1 = {'a', 'b'}
+        set_2 = {'a', 'b', 'c'}
+        set_3 = {''}
+        set_4 = {'a'}
+        set_5 = {}
+        self.assertEqual({
+            frozenset({}),
+            frozenset({'a'}), frozenset({'b'}),
+            frozenset({'a', 'b'})
+            }, powerset(set_1))
+        self.assertEqual({
+            frozenset({}),
+            frozenset({'a'}), frozenset({'b'}), frozenset({'c'}),
+            frozenset({'a', 'b'}), frozenset({'a', 'c'}), frozenset({'b', 'c'}),
+            frozenset({'a', 'b', 'c'})
+            }, powerset(set_2))
+        self.assertEqual({
+            frozenset({}),
+            frozenset({''}),
+            }, powerset(set_3))
+        self.assertEqual({
+            frozenset({}),
+            frozenset({'a'})
+            }, powerset(set_4))
+        self.assertEqual({frozenset({})}, powerset(set_5))
 
 if __name__ == '__main__':
     unittest.main()
