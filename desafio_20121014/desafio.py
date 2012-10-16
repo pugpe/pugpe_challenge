589d5c52b015df2b121f27ac7ef13f4614cc8897
Renato Oliveira <renatooliveira.cin@gmail.com>
2012-10-15 21:26:22 -0300

    Removendo os sets

--- desafio_20121014/desafio.py
+++ desafio_20121014/desafio.py
@@ -3,16 +3,16 @@ Desafio PUG-PE
 ID: 7
 SEMANA: 15/10/2012
 
-Problema: Achar o conjunto de todas as partições de um dado conjunto de strings.
+Problema: Achar todas as partições de uma dada lista de strings.
     Ex:
     >>> x = ['a', 'b']
-    >>> ret = subset(x)
+    >>> ret = partitions(x)
     >>> ret
-    >>> ['', a', 'b', ('a', 'b')]
+    >>> [[''], ['a'], ['b'], ['a', 'b']]
     >>> y = ['a', 'b', 'c']
-    >>> ret = subset()
+    >>> ret = partitions()
     >>> ret
-    >>> ['', 'a', 'b', 'c', ('a', 'b'), ('a', 'c'), ('b', 'c'), ('a', 'b', 'c')]
+    >>> [[''], ['a'], ['b'], ['c'], ['a', 'b'], ['a', 'c'], ['b', 'c'], ['a', 'b', 'c']]
 
     Favor utilizar Testes usando doctest ou UnitTest para validar sua solucao.
 """
@@ -28,9 +28,9 @@ class Desafio7(unittest.TestCase):
         set_3 = ['']
         set_4 = ['a']
         set_5 = []
-        self.assertEqual(set([('',), ('a',), ('b',), ('a', 'b')]), subset(set_1))
-        self.assertEqual(set([('',),('a',), 'b', 'c', ('a', 'b'), ('a', 'c'), ('b', 'c'), ('a', 'b', 'c')]), subset(set_2))
-        self.assertEqual(set([('',)]), subset(set_3))
-        self.assertEqual(set([('',),('a',)]), subset(set_4))
-        self.assertEqual(set([]), subset(set_5))
+        self.assertEqual([[''], ['a'], ['b'], ['a', 'b']], subset(set_1))
+        self.assertEqual([[''], ['a'], ['b'], ['c'], ['a', 'b'], ['a', 'c'], ['b', 'c'], ['a', 'b', 'c']], subset(set_2))
+        self.assertEqual([''], subset(set_3))
+        self.assertEqual(['', ['a']], subset(set_4))
+        self.assertEqual([], subset(set_5))
 
